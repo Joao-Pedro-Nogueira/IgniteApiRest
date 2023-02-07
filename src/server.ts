8309bd272a23749676e560198192a2c1a27bdeb8
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-02-07 18:25:25 -0400

    Migrations criadas, tabela de banco de dados, variáveis ambiente e validação com zod

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
+// import crypto from 'crypto'
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transaction = await await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
 
-  return tables
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.SERVER_PORT,
   })
   .then(() => {
     console.log('Server running!')
